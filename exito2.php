7124aaa9aa6dfda5dfc0948710d0d990cf72ac31
stillIsaac <isaacjanica@gmail.com>
2024-05-09 15:27:27 -0500

    added the notebook

--- exito2.php
+++ exito2.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <!-- Coding By CodingNepal - codingnepalweb.com -->
-<html lang="en" dir="ltr">
+<html lang="es" dir="ltr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,8 +13,8 @@
 <body>
 <div class="wrapper">
     <h2>Registro Exitoso</h2>
-    <h3> El registro se hizo con exito.
-        Dentro de poco las autoridades activaran la alarma is se estaran acercando al tunel.
+    <h3> El registro se hizo con éxito.
+        Dentro de poco las autoridades recibirán la alarma y se estarán acercando al túnel.
     </h3>
 </div>
 </body>
